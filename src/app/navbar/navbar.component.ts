433356608bd884b4c60df79ff5c8ae10ad6d07d3
mathieu <me94110@gmail.com>
2025-04-21 22:26:03 +0200

    responsive navbar menu done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,21 +1,44 @@
-import { ChangeDetectorRef, Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { Router, RouterModule } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  query,
+  stagger
+} from '@angular/animations';
+import { NAV_ITEMS } from "@app/constants/navbar";
 
 @Component({
   selector: "app-navbar",
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.scss",
+  animations: [
+    trigger('listStagger', [
+      transition('* => *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(20px)' }),
+          stagger('100ms', [
+            animate('300ms ease-out', style({ opacity: 1, transform: 'translateY(0)' })),
+          ]),
+        ], { optional: true })
+      ])
+    ])
+  ]
 })
 export class NavbarComponent implements OnInit {
   isInCompanyModules = false;
   isInRocketsModules = false;
   isInLaunchesModules = false;
+  isMenuOpen = false;
+  navItems = NAV_ITEMS;
 
   constructor(
     private readonly router: Router,
-    private readonly cdRef: ChangeDetectorRef,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.checkUrl();
@@ -28,5 +51,9 @@ export class NavbarComponent implements OnInit {
     this.isInCompanyModules = this.router.url.includes("company");
     this.isInRocketsModules = this.router.url.includes("rockets");
     this.isInLaunchesModules = this.router.url.includes("launches");
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
