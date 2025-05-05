3508a1427b9a7ef53c9d7ffd3264293e5de0f4b5
mathieu <me94110@gmail.com>
2025-05-06 00:05:50 +0200

    add integration tests for pagination layout, tests for search capsule component on progress

--- src/app/components/capsules/search-capsule/search-capsule.component.spec.ts
+++ src/app/components/capsules/search-capsule/search-capsule.component.spec.ts
@@ -1,23 +1,142 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, flush, tick } from '@angular/core/testing';
 import { SearchCapsuleComponent } from './search-capsule.component';
+import { CapsulesService } from 'services/capsules.service';
+import { of, throwError } from 'rxjs';
+import { mockCapsules } from 'mocks/capsules';
+import { ICapsule } from 'interfaces/capsules';
+import { Component, Input, signal } from '@angular/core';
 
 describe('SearchCapsuleComponent', () => {
-  let component: SearchCapsuleComponent;
   let fixture: ComponentFixture<SearchCapsuleComponent>;
+  let component: SearchCapsuleComponent;
+  let capsulesService: CapsulesService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SearchCapsuleComponent]
-    })
-    .compileComponents();
+  beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      imports: [SearchCapsuleComponent],
+      providers: [CapsulesService],
+    });
 
     fixture = TestBed.createComponent(SearchCapsuleComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    capsulesService = TestBed.inject(CapsulesService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  /*   it('should show loading spinner when loading is true', () => {
+      capsulesService.searchCapsuleResource.setLoading(true);
+      fixture.detectChanges();
+  
+      const spinner = fixture.nativeElement.querySelector('mat-spinner');
+      expect(spinner).toBeTruthy();
+    }); */
+  it('should debounce search input and only call service once', fakeAsync(() => {
+    spyOn(capsulesService, 'setCapsuleResearch');
+    // user typed on input
+    component.searchTerm.set('C');
+    tick(100);
+    component.searchTerm.set('Ca');
+    tick(100);
+    component.searchTerm.set('Cap');
+    tick(100);
+    component.searchTerm.set('Capsule');
+
+    // Wait for the full debounce delay
+    tick(400);
+
+    fixture.detectChanges();
+    flush();
+
+    expect(capsulesService.setCapsuleResearch).toHaveBeenCalledTimes(1);
+    expect(capsulesService.setCapsuleResearch).toHaveBeenCalledWith('Capsule');
+  }));
+  it('should reset all search-related states after call to resetSearchCapsules()', () => {
+    component.searchTerm.set('Test');
+    component.hasSearched.set(true);
+    component.pendingSearchTerm.set('Test');
+    component.hasLoadedOnce.set(true);
+    capsulesService.searchCapsuleResource.set({
+      docs: mockCapsules,
+      totalDocs: 3,
+      totalPages: 1,
+    });
+
+    component.resetSearchCapsules();
+
+    expect(component.hasSearched()).toBeFalse();
+    expect(component.pendingSearchTerm()).toBe('');
+    expect(component.searchCapsulesList()).toEqual([]);
+  });
+  it('should show no result message when appropriate', fakeAsync(() => {
+    component.searchTerm.set('invalid search');
+    component.lastCompletedSearchTerm.set('invalid search');
+    component.hasSearched.set(true);
+    component.hasLoadedOnce.set(true);
+    // searchCapsuleResource has no data on docs meaning that the API call did not retrieve any data
+    capsulesService.searchCapsuleResource.set({ docs: [], totalDocs: 0, totalPages: 0 });
+
+    tick(500);
+    fixture.detectChanges();
+
+    expect(component.showNoResultsMessage()).toBeTrue();
+  }));
 });
+
+
+/*
+ 
+    it('should display capsule cards when search results are available', fakeAsync(() => {
+  // Simule la valeur retournée par l'API avec un observable
+  /*     capsulesServiceSpy.searchCapsuleResource = {
+        ...capsulesServiceSpy.searchCapsuleResource,
+        value: of({
+          docs: mockCapsules,
+          totalDocs: 3,
+          totalPages: 1
+        }),
+      };
+ 
+      component.searchTerm.set('C1');
+      tick(500); 
+      fixture.detectChanges();
+ 
+      const cards = fixture.nativeElement.querySelectorAll('.mock-search-card');
+      expect(cards.length).toBe(4);
+    }));
+it('should display no result message when search returns empty', fakeAsync(() => {
+    capsulesServiceSpy.searchCapsuleResource.value = () => ({
+      docs: [],
+      totalDocs: 0,
+      totalPages: 0
+    });
+ 
+    component.searchTerm.set('XXXX');
+    component.lastCompletedSearchTerm.set('XXXX');
+    component.hasSearched.set(true);
+    component.hasLoadedOnce.set(true);
+ 
+    tick(500);
+    fixture.detectChanges();
+ 
+    const noResult = fixture.nativeElement.querySelector('.no-results-message');
+    expect(noResult).toBeTruthy();
+    expect(noResult.textContent).toContain('Aucun résultat trouvé');
+  }));
+ 
+  it('should display an error message when service fails', fakeAsync(() => {
+    capsulesServiceSpy.searchCapsuleResource.error = () => ({ message: 'Erreur serveur' });
+ 
+    component.searchTerm.set('C1');
+    component.hasSearched.set(true);
+    component.hasLoadedOnce.set(true);
+ 
+    tick(500);
+    fixture.detectChanges();
+ 
+    const errorEl = fixture.nativeElement.querySelector('.error');
+    expect(errorEl).toBeTruthy();
+    expect(errorEl.textContent).toContain('Erreur serveur');
+  })); */
--- src/app/layouts/pagination-layout/pagination-layout.component.spec.ts
+++ src/app/layouts/pagination-layout/pagination-layout.component.spec.ts
@@ -1,23 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PaginationLayoutComponent } from './pagination-layout.component';
+import { ComponentRef } from '@angular/core';
 
 describe('PaginationLayoutComponent', () => {
   let component: PaginationLayoutComponent;
   let fixture: ComponentFixture<PaginationLayoutComponent>;
+  let componentRef: ComponentRef<PaginationLayoutComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PaginationLayoutComponent]
-    })
-    .compileComponents();
+      imports: [PaginationLayoutComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PaginationLayoutComponent);
+    componentRef = fixture.componentRef;
+    componentRef.setInput('hasData', true);
+    componentRef.setInput('isPrevButtonVisible', true);
+    componentRef.setInput('isNextButtonVisible', true);
+
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
+
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+  it('should display .pagination when hasData is true', () => {
+    const paginationDiv = nativeElement.querySelector('.pagination');
+    expect(paginationDiv).toBeTruthy();
+  });
+  it('should not display .pagination when hasData is false', () => {
+    componentRef.setInput('hasData', false);
+    fixture.detectChanges();
+
+    const paginationDiv = nativeElement.querySelector('.pagination');
+    expect(paginationDiv).toBeNull();
+  });
+  it('should display 2 buttons, first with text Précédent second with text Suivant', () => {
+    const buttons = nativeElement.querySelectorAll('button');
+    expect(buttons.length).toBe(2);
+    expect(buttons[0]?.textContent).toContain('Précédent');
+    expect(buttons[1]?.textContent).toContain('Suivant');
+  });
+  it('should show only one button when only next is visible', () => {
+    componentRef.setInput('isPrevButtonVisible', false);
+    componentRef.setInput('isNextButtonVisible', true);
+    fixture.detectChanges();
+    const buttons = nativeElement.querySelectorAll('button');
+    const prevButton = Array.from(buttons).find(btn => btn.textContent?.includes('Précédent'));
+    const nextButton = Array.from(buttons).find(btn => btn.textContent?.includes('Suivant'));
+    expect(buttons.length).toBe(1);
+    expect(prevButton).toBeUndefined();
+    expect(nextButton).toBeDefined();
+    expect(nextButton?.textContent).toContain('Suivant');
+  });
+
+  it('should emit nextClicked when nextPage is called', () => {
+    spyOn(component.nextClicked, 'emit');
+    component.nextPage();
+    expect(component.nextClicked.emit).toHaveBeenCalled();
+  });
+})
