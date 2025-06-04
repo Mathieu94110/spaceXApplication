ef90657f09bf81ca8a6173f5964c09093486e5e6
mathieu <me94110@gmail.com>
2025-06-05 00:05:02 +0200

    fix: broken category in HOME_SECTION_ITEMS

--- src/app/constants/home.ts
+++ src/app/constants/home.ts
@@ -2,7 +2,7 @@ export const HOME_SECTION_ITEMS = [
   { title: 'starlink mission', subtitle: "lancements", category: 'launches', link: "/launches", textButton: 'regarder' },
   { title: 'starlink mission', subtitle: "capsules", category: 'capsules', link: "/capsules", textButton: 'regarder' },
   { title: 'starlink mission', subtitle: "dragons", category: 'dragons', link: "/dragons", textButton: 'regarder' },
-  { title: 'starlink mission', subtitle: "plateformes de lancement", category: 'launch pads', link: "/launchpads", textButton: 'regarder' },
+  { title: 'starlink mission', subtitle: "plateformes de lancement", category: 'launchpad', link: "/launchpads", textButton: 'regarder' },
   { title: 'starlink mission', subtitle: "missions", category: 'crew', link: "/crew", textButton: 'regarder' },
 ];
 
